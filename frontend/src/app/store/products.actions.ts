66b5c516d787c33da63fc246e4de519f53554957
Bakytai Egemberdieva <bakijw@gmail.com>
2022-03-12 16:59:51 +0600

    Прописала actions для Products

---
+++ frontend/src/app/store/products.actions.ts
@@ -0,0 +1,47 @@
+import { createAction, props } from '@ngrx/store';
+import { Product, ProductData } from '../models/product.model';
+
+export const fetchProductsRequest = createAction('[Products] Fetch Request');
+export const fetchProductsSuccess = createAction(
+  '[Products] Fetch Success',
+  props<{products: Product[]}>()
+);
+export const fetchProductsFailure = createAction(
+  '[Products] Fetch Failure',
+  props<{error: string}>()
+);
+
+export const fetchProductRequest = createAction('[Product] Fetch Request');
+export const fetchProductSuccess = createAction(
+  '[Product] Fetch Success',
+  props<{product: Product}>()
+);
+export const fetchProductFailure = createAction(
+  '[Product] Fetch Failure',
+  props<{error: string}>()
+);
+
+export const createProductRequest = createAction(
+  '[Products] Create Request',
+  props<{productData: ProductData, token: string}>()
+);
+export const createProductSuccess = createAction(
+  '[Products] Create Success'
+);
+export const createProductFailure = createAction(
+  '[Products] Create Failure',
+  props<{error: string}>()
+);
+
+export const deleteProductRequest = createAction(
+  '[Products] Create Request',
+  props<{id: string, token: string}>()
+);
+export const deleteProductSuccess = createAction(
+  '[Products] Create Success'
+);
+export const deleteProductFailure = createAction(
+  '[Products] Create Failure',
+  props<{error: string}>()
+);
+
